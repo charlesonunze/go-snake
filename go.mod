558e07c8348916555f5dc79d4693f1857a4c2933
Charles Onunze <charlesonunze2@gmail.com>
2021-11-07 22:43:40 +0100

    feat: paint game board

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/charlesonunze/go-snake
+
+go 1.16
+
+require github.com/veandco/go-sdl2 v0.4.10
---
+++ main.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/veandco/go-sdl2/sdl"
+)
+
+const (
+	winTitle   = "Go Snake"
+	boardColor = 0xffCC98
+	winWidth,
+	winHeight,
+	maxWidth,
+	maxHeight,
+	cellWidth,
+	cellHeight int32 = 1400, 800, 1080, 600, 20, 20
+)
+
+func run() error {
+	var w, h int
+
+	flag.IntVar(&w, "w", 1080, "width of the game board")
+	flag.IntVar(&h, "h", 600, "height of the game board")
+	flag.Parse()
+
+	boardWidth, boardHeight := int32(w), int32(h)
+	if boardWidth == 0 || boardHeight == 0 {
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	err := sdl.Init(sdl.INIT_EVERYTHING)
+	handleErr("could not initialize SDL:", err)
+	defer sdl.Quit()
+
+	window, err := sdl.CreateWindow(
+		winTitle,
+		sdl.WINDOWPOS_UNDEFINED,
+		sdl.WINDOWPOS_UNDEFINED,
+		winWidth,
+		winHeight,
+		sdl.WINDOW_SHOWN)
+	handleErr("failed to create window:", err)
+	defer window.Destroy()
+
+	surface, err := window.GetSurface()
+	handleErr("could not get surface:", err)
+
+	middlePositionX := int32((winWidth - boardWidth) / 2)
+	middlePositionY := int32((winHeight - boardHeight) / 2)
+
+	posX, posY := middlePositionX, middlePositionY
+
+	xCells := int(boardWidth / cellWidth)
+	yCells := int(boardHeight / cellHeight)
+
+	board := make([][]sdl.Rect, yCells)
+
+	for i := range board {
+		for j := 0; j < xCells; j++ {
+			rect := &sdl.Rect{
+				X: posX,
+				Y: posY,
+				W: cellWidth,
+				H: cellHeight,
+			}
+
+			surface.FillRect(rect, boardColor)
+
+			board[i] = append(board[i], *rect)
+			posX += cellWidth
+		}
+
+		posX = middlePositionX
+		posY += cellHeight
+	}
+
+	window.UpdateSurface()
+
+	running := true
+	for running {
+		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
+			switch event.(type) {
+			case *sdl.QuitEvent:
+				running = false
+			}
+		}
+	}
+
+	return nil
+}
+
+func handleErr(msg string, err error) error {
+	if err != nil {
+		return fmt.Errorf(msg+" %v", err)
+	}
+	return nil
+}
+
+func main() {
+	if err := run(); err != nil {
+		fmt.Fprintf(os.Stderr, "%v", err)
+		os.Exit(2)
+	}
+}
